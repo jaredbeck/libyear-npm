fce02d71f0e8b58e0ce354c228620a4f3cddca73
Peter Morlion <peter.morlion@gmail.com>
2019-12-19 20:33:57 +0100

    Allow calculator to be used programmatically
    This commit changes calculator to return an object with the result. This allows for programmatic use, i.e. using this package in your application and doing with the result whatever you want. For this, I built up an object with the results. But because we want to keep seeing the output as it is calculated, I introduced an event emitter to which the main function subscribes. It can then write to stdout as the results are coming in.
    
    I've also chosen to throw errors and handle everything in the main function (instead of using stdout and exiting). In case someone uses this as a library in their application, it's up to them to handle errors.

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,53 +1,53 @@
-describe("calculator", function() {
-  var calculator = require('../lib/calculator.js');
+describe('calculator', function() {
+  var calculator = require('../lib/calculator.js').calculator;
   var moment = require('moment');
 
-  it("is a function", function() {
-    expect(typeof(calculator)).toEqual("function");
+  it('is a function', function() {
+    expect(typeof(calculator)).toEqual('function');
   });
 
-  describe("nothing is outdated", function() {
-    it("prints a total of 0.0", function() {
+  describe('nothing is outdated', function() {
+    it('returns a total of 0.0', function() {
       var outdated = '';
       var releaseTime = function() {
-        throw "test failed: unexpected call"
+        throw 'test failed: unexpected call'
       };
-      var stdout = { write: function() {} };
-      spyOn(stdout, 'write');
-      calculator(outdated, releaseTime, stdout);
-      expect(stdout.write).toHaveBeenCalledWith(
-        'System is 0.0 libyears behind\n'
-      );
+      var result = calculator(outdated, releaseTime);
+      expect(result.totalLibYears).toEqual(0);
     });
   });
 
-  describe("a package is outdated", function() {
-    it("prints the expected output", function() {
+  describe('a package is outdated', function() {
+    it('prints the expected output', function() {
       var outdated = JSON.stringify(
         {
-          "banana": {
-            "current": "1.0.0",
-            "wanted": "1.0.1",
-            "latest": "2.0.0",
-            "location": "node_modules/banana"
+          'banana': {
+            'current': '1.0.0',
+            'wanted': '1.0.1',
+            'latest': '2.0.0',
+            'location': 'node_modules/banana'
           }
         }
       );
       var releaseTime = function(packageName, version) {
         return {
-          "1.0.0": moment("2016-02-28"),
-          "1.0.1": moment("2016-02-29"),
-          "2.0.0": moment("2017-02-28")
+          '1.0.0': moment('2016-02-28'),
+          '1.0.1': moment('2016-02-29'),
+          '2.0.0': moment('2017-02-28')
         }[version];
       };
-      var stdout = { write: function() {} };
-      spyOn(stdout, 'write');
-      calculator(outdated, releaseTime, stdout);
-      expect(stdout.write).toHaveBeenCalledWith(
-        '                        banana      1.0.0      2016-02-28' +
-        '      2.0.0      2017-02-28    1.00\n'
-      );
-      expect(stdout.write).toHaveBeenCalledWith('System is 1.0 libyears behind\n');
+      var result = calculator(outdated, releaseTime);
+      expect(result).toEqual({
+        banana: {
+          key: 'banana',
+          currentVersion: '1.0.0',
+          currentMoment: moment('2016-02-28'),
+          latestVersion: '2.0.0',
+          latestMoment: moment('2017-02-28'),
+          years: 1
+        },
+        totalLibYears: 1
+      });
     });
   });
 });
